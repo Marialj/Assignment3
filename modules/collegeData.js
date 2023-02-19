7dfac3451af098c87fe73f52a547ccb9b0ae4300
Maria Joy <mariajoylj@gmail.com>
2023-02-18 19:28:55 -0500

    Create collegeData.js

---
+++ modules/collegeData.js
@@ -0,0 +1,138 @@
+var fs = require('fs');
+
+class Data {
+    constructor(students,courses){
+        this.students = students;
+        this.courses = courses;
+    }
+}
+
+var dataCollection = null;
+
+function initialize() {
+
+    return new Promise( (resolve, reject) => {
+        fs.readFile('./data/students.json','utf8', (err, studentFile) => {
+            if (err) {
+                reject("unable to read student file"); return;
+            }
+
+            fs.readFile('./data/courses.json','utf8', (err, courseFile) => {
+                if (err) {
+                    reject("unable to read course file"); return;
+                }
+                dataCollection = new Data(JSON.parse(studentFile), JSON.parse(courseFile));
+                resolve()
+            });
+        });
+    });
+}
+
+
+function getAllStudents() {
+
+    return new Promise((resolve,reject) => {
+
+        var studentData = dataCollection.students
+
+        if(studentData.length == 0){
+            reject('no results returned')
+        }
+        else {
+            resolve(studentData)
+        }
+                
+    })
+
+}
+
+function getTAs() {
+
+    return new Promise((resolve,reject) =>{
+
+        var taList = []
+        var studentData = dataCollection.students
+
+        for(let i =0; i < studentData.length; i++ ){
+            if(studentData[i].TA == true){
+                taList.push(studentData[i])
+            }
+
+        }
+        
+        if (taList.length == 0){
+            reject('no results returned')
+        }
+        else {
+            resolve(taList)
+        }
+
+    })
+}
+
+function getCourses() {
+
+    return new Promise((resolve,reject) =>{
+        var courseData = dataCollection.courses
+
+        if (courseData.length == 0){
+            reject('no results returned')
+        }
+        else {
+            resolve(courseData)
+        }
+
+    })
+
+}
+
+function getStudentsByCourse(course) {
+
+    return new Promise((resolve,reject) =>{
+
+        var stuList = []
+        var studentData = dataCollection.students
+
+        for(let i =0; i < studentData.length; i++ ){
+            if(studentData[i].course == course){
+                stuList.push(studentData[i])
+            }
+
+        }
+        
+        if (stuList.length == 0){
+            reject('no results returned')
+        }
+        else {
+            resolve(stuList)
+        }
+
+    })
+}
+
+function getStudentByNum(num) {
+
+    return new Promise((resolve,reject) =>{
+
+        var stuName
+        var studentData = dataCollection.students
+
+        for(let i =0; i < studentData.length; i++ ){
+            if(studentData[i].studentNum == num){
+                stuName = studentData[i].firstName+" "+studentData[i].lastName     
+            }
+
+        }
+         
+        if (stuName == null){
+            reject('no results returned')
+        }
+        else {
+            resolve(stuName)
+        }
+
+    })
+}
+
+
+module.exports = {initialize,getAllStudents,getTAs,getCourses,getStudentsByCourse,getStudentByNum}
